e40dd1f99bbc5da283fc5973a5c763be61128b22
H1k4 <145106093+H1k4@users.noreply.github.com>
2024-01-19 10:58:32 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
-var name = "Hikari";
-let name1 = "irakiH";
-// alert(name1);
-// let age = prompt("lu li la", 18)
-console.log(99 + 55, '1'+ 1, 1 + +'2', '3' + '1');
+
+let h1 = document.getElementsByTagName("h1");
+console.log(h1);
+let h2Mass = document.getElementsByTagName("h3");
+console.log(h2Mass);
+h2Mass[0].innerText = "CalaMaleikum";
+let classElem = document.getElementsByClassName("header-menu-item");
+classElem[classElem.length - 1].innerHTML = "ABOBA";
+let work = document.getElementById("work");
+let education = document.getElementById("education");
+console.log(work, education);
+let Blyad = document.querySelector(".header-menu-item");
+console.log(Blyad);
+work.style.bColor = "rgba(75,130,200)";
+work.onclick = function () {
+    education.style.display = "flex";
+    console.log(this);
+};
+education.onclick = () => {
+    console.log(this);
+};
