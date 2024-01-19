e35b380def1f5048077857cf70f9195a206bdd81
H1k4 <145106093+H1k4@users.noreply.github.com>
2024-01-19 10:55:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-let name = "Hikari";
-let age = 15;
-let boolean = true;
-let Nixuya = null;
-let Suka = undefined;
-let people = {
-    name: "Keiko",
-    age: 18,
-    pizdish: true,
-    children: {},
+
+let h1 = document.getElementsByTagName("h1");
+console.log(h1);
+let h2Mass = document.getElementsByTagName("h3");
+console.log(h2Mass);
+h2Mass[0].innerText = "CalaMaleikum";
+let classElem = document.getElementsByClassName("header-menu-item");
+classElem[classElem.length - 1].innerHTML = "ABOBA";
+let work = document.getElementById("work");
+let education = document.getElementById("education");
+console.log(work, education);
+let Blyad = document.querySelector(".header-menu-item");
+console.log(Blyad);
+work.style.bColor = "rgba(75,130,200)";
+work.onclick = function () {
+    education.style.display = "flex";
+    console.log(this);
+};
+education.onclick = () => {
+    console.log(this);
 };
