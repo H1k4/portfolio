6a764ee8303984dcb198dcc3ef30ff2145ad6045
H1k4 <145106093+H1k4@users.noreply.github.com>
2024-02-01 23:23:33 +1000

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,19 +3,23 @@ let work = document.getElementById("work");
 let list = document.querySelector(".qualific-list");
 const massCards = [
    {
-    name: "programmist",
-    univer:"Spain",
-    data:"20",
+    name: "Web Design",
+    univer:"Spain-Institute",
+    data:"2014-2017",
+    number:"20",
+    text:"Completed projects",
   },
    {
-    name: "vbgggf",
-    univer:"Srdf",
-    data:"2045",
+    name: "Web Development",
+    univer:"Peru-Institute",
+    data:"2017-2019",
+    number:"4",
+    text:"Companies worked",
   },
    {
-    name: "vbgggf",
-    univer:"Srdf",
-    data:"2045",
+    name: "Master UI/ UX",
+    univer:"Peru-Istitute",
+    data:"2019-2021",
   },
 ];
 for (let i = 0; i < massCards.length; i++) {
@@ -28,6 +32,16 @@ for (let i = 0; i < massCards.length; i++) {
       <img src="./assets/images/data.svg" alt="">
     <h4>${massCards[i].data}</h4>
   </article>`);
+}
+console.log(list);
+for (let i = 0; i < massCards.length; i++) {
+  console.log(massCards[i]);
+  list.insertAdjacentHTML("afterbegin",` <article class="info-list-card">
+  <div class="info-list-card-bottom">
+  <h5>${massCards[i].name}</h5>
+  <h1>${massCards[i].name}</h1>
+</div>
+</article>`);
 }
 console.log(list);
 
